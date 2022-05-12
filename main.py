6a9e6aa72778df0725e069b594910dd097c41817
Amoongus <100391354+20195e@users.noreply.github.com>
2022-05-12 01:37:50 +0000

    jade deleted all my stuff not very poggers

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # Te Reo Quiz - Vincent Beet
+score = 0
+question = input("Welcome the Te Reo Quiz, Are you ready to begin? ").lower()
+
+if question == "yes":
+  print("Question one!")
+
+question1 = input("1. What does Pero mean in english? A. Dog? B. Sparrow? C. Lizard? ").lower()
+
+if question1 == "A" or "Dog":
+  score += 1
+  print("Correct.")
+  print("Score:", score)
+ 
+else:
+  print("Incorrect.")
+  print("Score:", score)
+
