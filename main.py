29d864a391e4605645a42ef70fc5c8825372d14d
Amoongus <100391354+20195e@users.noreply.github.com>
2022-05-30 23:15:50 +0000

    Added questions and ending + beginning

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def question1():
 
   elif ans1 == "true":
     print("Incorrect")
+    question2()
 
   else:
     print("Please choose True or False")
@@ -27,12 +28,77 @@ def question2():
     print("Correct")
     score += 1
     print("Score:",score)
+    question3()
 
   elif ans2 == "true":
     print("Incorrect")
+    question3()
+
+  else:
+    print("Please choose True or False")
+    question2()
 
 
+def question3():
+  global score
+  print("This is question Three")
+  ans3 = input("Mane means Monday in English! True or False?").lower()
 
+  if ans3 == "true":
+    print("Correct")
+    score += 1
+    print("Score:",score)
+    question4()
+
+  elif ans3 == "false":
+    print("Incorrect")
+    question4()
+
+  else:
+    print("Please choose True or False")
+    question3()
+
+
+def question4():
+  global score
+  print("This is question Four")
+  ans4 = input("heihei means a group of two kids in English! True or False?").lower()
+
+  if ans4 == "false":
+    print("Correct")
+    score += 1
+    print("Score:",score)
+    question5()
+
+  elif ans4 == "true":
+    print("Incorrect")
+    question5()
+
+  else:
+    print("Please choose True or False")
+    question4()
+
+
+def question5():
+  global score
+  print("This is question Five")
+  ans5 = input("Iwa means 6 in English! True or False?").lower()
+
+  if ans5 == "false":
+    print("Correct")
+    score += 1
+    print("Score:",score)
+    print("The quiz is over you have scored",score)
+    print("points, Congratulations!")
+
+  elif ans5 == "true":
+    print("Incorrect")
+
+  else:
+    print("Please choose True or False")
+    question5()
 
 score = 0
+print("This is a quiz meant to test your basic knowledge of Te Reo")
+print("\n")
 question1()
